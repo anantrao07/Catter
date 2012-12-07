5caa4d0b33e270a72485db2ff6c36e6c36a0aae8
rzeszut <rzeszutekm@gmail.com>
2012-12-07 13:06:49 +0100

    methods for adding high scores added

--- src/edu/sis/catter/HighScoresActivity.java
+++ src/edu/sis/catter/HighScoresActivity.java
@@ -1,24 +1,30 @@
 package edu.sis.catter;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.SurfaceView;
 
 
 public class HighScoresActivity extends Activity {
-    private static final String DEFAULT_NAME = "---";
-    private static final int DEFAULT_SCORE = 0;
+    public static final String CATTER_PREFS_NAME = "catter_prefs";
+    public static final String CATTER_SCORE_PREFIX = "catter_score_";
+    public static final String CATTER_NAME_PREFIX = "catter_name_";
+    public static final String DEFAULT_NAME = "---";
+    public static final int DEFAULT_SCORE = 0;
 
     private HighScoresPanel scoresPanel;
     private List<String> scores = new ArrayList<String>();
@@ -33,16 +39,19 @@ public class HighScoresActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-        SharedPreferences prefs = getSharedPreferences("catter_prefs",
+        SharedPreferences prefs = getSharedPreferences(CATTER_PREFS_NAME,
                 Context.MODE_PRIVATE);
         for (int i = 0; i < 10; ++i) {
             StringBuffer text = new StringBuffer();
-            text.append(i + 1).append(". ")
-                    .append(prefs.getString("catter_name_" + i, DEFAULT_NAME))
+            text.append(i + 1)
+                    .append(". ")
+                    .append(prefs.getString(CATTER_NAME_PREFIX + i,
+                            DEFAULT_NAME))
                     .append(": ")
-                    .append(prefs.getInt("catter_score_" + i, DEFAULT_SCORE));
+                    .append(prefs
+                            .getInt(CATTER_SCORE_PREFIX + i, DEFAULT_SCORE));
             scores.add(text.toString());
-            Log.d("scores", text.toString());
+            Log.d("catter high scores", text.toString());
         }
     }
 
@@ -59,7 +68,6 @@ public class HighScoresActivity extends Activity {
             paint.setAntiAlias(true);
         }
 
-        @SuppressLint("DrawAllocation")
         @Override
         public void onDraw(Canvas c) {
             c.drawColor(Color.BLACK);
@@ -69,5 +77,49 @@ public class HighScoresActivity extends Activity {
                 c.drawText(s, 100, 80 + 30 * i++, paint);
             }
         }
+    }
+
+    // TODO paste those two functions below wherever they are needed
+    public boolean isScoreBigEnough(int score) {
+        SharedPreferences prefs = getSharedPreferences(CATTER_PREFS_NAME,
+                Context.MODE_PRIVATE);
+        int lowest = prefs.getInt("catter_score_9", DEFAULT_SCORE);
+        return score > lowest;
+    }
+
+    public void addHighScore(String name, int score) {
+        List<Pair<String, Integer>> scoresList = new ArrayList<Pair<String, Integer>>();
+        SharedPreferences prefs = getSharedPreferences(CATTER_PREFS_NAME,
+                Context.MODE_PRIVATE);
+        for (int i = 0; i < 10; ++i) {
+            scoresList.add(Pair.create(
+                    prefs.getString(CATTER_NAME_PREFIX + i, DEFAULT_NAME),
+                    prefs.getInt(CATTER_SCORE_PREFIX + i, DEFAULT_SCORE)));
+        }
+        scoresList.add(Pair.create(name, score));
+
+        Collections.sort(scoresList, new Comparator<Pair<String, Integer>>() {
+
+            @Override
+            public int compare(Pair<String, Integer> lhs,
+                    Pair<String, Integer> rhs) {
+                if (lhs.second > rhs.second) {
+                    return -1;
+                } else if (lhs.second < rhs.second) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+
+        Editor e = prefs.edit();
+        int i = 0;
+        for (Pair<String, Integer> p : scoresList.subList(0, 10)) {
+            e.putString(CATTER_NAME_PREFIX + i, p.first);
+            e.putInt(CATTER_SCORE_PREFIX + i, p.second);
+            ++i;
+        }
+        e.commit();
     }
 }
